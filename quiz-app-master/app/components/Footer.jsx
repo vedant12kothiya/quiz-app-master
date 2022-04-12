644cd57de702c3661c5f6bdfb168cfdc048cc2c0
vedant12kothiya <97938448+vedant12kothiya@users.noreply.github.com>
2022-04-12 10:40:45 +0530

    Add files via upload

---
+++ quiz-app-master/app/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer>
+                <small>
+                    You can find the code repo on <a href="https://github.com/Flopet17/quiz-app">Github</a>. Feel free to make any pull requests to improve the aplication.
+                </small>
+                <p>Built with <span>&hearts;</span> by <a href="http://florin-pop.com">Florin Pop</a></p>
+            </footer>
+        )
+    }
+}
+
+export default Footer
